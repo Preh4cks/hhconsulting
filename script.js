e7beded0858be0617f8555aa50daebcf3138373a
Arjhay Frias <prehacks0942@gmail.com>
2023-09-03 21:41:14 +0800

    Added new Sections

--- script.js
+++ script.js
@@ -9,5 +9,25 @@
       speed: 1600,
       arrows: false,
     });
+
+    $('.projects_carousel').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 1600,
+      arrows: false,
+    });
+
+    $('.services_carousel').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 1600,
+      arrows: false,
+    });
   });
 })();
